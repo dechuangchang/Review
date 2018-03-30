09048dd75e4e4d5157bfd416a5ca35e26185aa86
changdechuang <15264086654@163.com>
2018-03-30 13:47:12 +0800

    combineReducers

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
 class App extends Component {
+  
   render() {
+    
+    console.log(this.props.dispatch)
     return (
       <div className="App">
         {this.props.value}
@@ -12,11 +16,20 @@ class App extends Component {
     );
   }
 }
-const mapStateToProps = (state)=>{
-    console.log(state); // state
-    console.log(arguments[1]); // undefined
+
+//如果定义该参数，组件将会监听 Redux store 的变化。任何时候，只要 Redux store 发生改变，
+const mapStateToProps = (state,ownProps)=>{
+    console.log(state)
     return{
-        value:state
+        value:state.counter
     }
-}//把值传到props
+}
+
+// const mapDispatchToProps = (dispatch)=>{
+   
+//     return{
+//         dispatch:bindActionCreators(dispatch)
+//     }
+    
+// }
 export default connect(mapStateToProps)(App);
--- redux/src/index.js
+++ redux/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
-import counter from './reducers/counter.js';
+import rootReducer from './reducers/index.js';
 // import {add,ree} from './actions/index.js';
 import { Provider  } from 'react-redux';
-const store = createStore(counter);
+const store = createStore(rootReducer);
 ReactDOM.render(
     <Provider store={store}>
     <App store={store}/>
